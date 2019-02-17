0cc1a100c0934cd9acd4ed1aa5d85fd4b6821880
student <student@localhost>
2019-02-17 20:50:46 +0300

    everything works successfully

---
+++ src/main/java/ru/itpark/youblog/YoublogApplication.java
@@ -0,0 +1,13 @@
+package ru.itpark.youblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoublogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoublogApplication.class, args);
+    }
+
+}
